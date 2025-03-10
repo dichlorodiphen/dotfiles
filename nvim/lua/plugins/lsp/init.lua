386b13542ee1bdf77eed0a1534cf76773b646e29
dichlorodiphen <dichlorodiphen@gmail.com>
2025-03-10 03:08:23 +0000

    Preselect first option nvim-cmp

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -53,6 +53,12 @@ return {
             vim.fn["vsnip#anonymous"](args.body)
           end,
         },
+        preselect = cmp.PreselectMode.Item,
+        completion = { completeopt = "menu,menuone" },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
