51f25dd3cfa43a0f27e886ca6942be0d24afd52c
dichlorodiphen <dichlorodiphen@gmail.com>
2025-03-13 00:55:46 +0000

    Add clang-format

--- nvim/lua/plugins/general.lua
+++ nvim/lua/plugins/general.lua
@@ -1,95 +1,101 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd("colorscheme tokyonight")
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      options = {
-        theme = 'tokyonight'
-      },
-    },
-  },
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim"
-    },
-    config = function()
-      require("telescope").load_extension("file_browser")
-    end,
-    keys = {
-      { "<leader>fb", "<cmd>Telescope file_browser<cr>" },
-    }
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    keys = {
-      { "<leader>fs", "<cmd>Telescope find_files<cr>" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
-      { "<leader>bb", "<cmd>Telescope buffers<cr>" },
-      { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
-    },
-    opts = {
-      defaults = {
-        path_display = { "truncate" },
-      },
-      extensions = {
-        file_browser = {
-          display_stat = false,
-          depth = 3,
-          collapse_dirs = true,
-          grouped = true,
-        },
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-      require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = function()
-      -- If you want insert `(` after select function or method item
-      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-      local cmp = require('cmp')
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end
-  },
-  {
-    "aznhe21/actions-preview.nvim",
-    keys = {
-      { "<leader>aa", function() require("actions-preview").code_actions() end },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-      formatters_by_ft = {
-        lua = { "stylua" },
-      },
-    },
-  }
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			vim.cmd("colorscheme tokyonight")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			options = {
+				theme = "tokyonight",
+			},
+		},
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("telescope").load_extension("file_browser")
+		end,
+		keys = {
+			{ "<leader>fb", "<cmd>Telescope file_browser<cr>" },
+		},
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		keys = {
+			{ "<leader>fs", "<cmd>Telescope find_files<cr>" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+			{ "<leader>bb", "<cmd>Telescope buffers<cr>" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>" },
+		},
+		opts = {
+			defaults = {
+				path_display = { "truncate" },
+			},
+			extensions = {
+				file_browser = {
+					display_stat = false,
+					depth = 3,
+					collapse_dirs = true,
+					grouped = true,
+				},
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			require("nvim-treesitter.install").update({ with_sync = true })()
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = function()
+			-- If you want insert `(` after select function or method item
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
+	{
+		"aznhe21/actions-preview.nvim",
+		keys = {
+			{
+				"<leader>aa",
+				function()
+					require("actions-preview").code_actions()
+				end,
+			},
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				cpp = { "clang-format" },
+			},
+		},
+	},
 }
